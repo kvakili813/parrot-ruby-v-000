7485d2b02e23eb49c98a26c06a53d85a97c4f715
Kayvan Vakili <kvakili813@gmail.com>
2017-12-24 21:33:35 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,5 @@
-def parrot ("Squawk!")
-  puts
+def parrot (string)
+  puts string
 end# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+
+def parrot ("Squawk!")# returns the phrase
